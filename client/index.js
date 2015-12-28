34d3bfce7943e1434fe9c87895cc083545db5b84
Martin Rogalla <martin.rogalla@magnet.me>
2015-12-28 10:18:20 +0100

    Added secret key notice for crawlers.
    Signed-off-by: Martin Rogalla <martin.rogalla@magnet.me>

--- client/index.js
+++ client/index.js
@@ -16,7 +16,7 @@ var getClientId = function(callback) {
 	request.post({
 		url : evansURL + '/client/register',
 		json : {
-			'secret' : 'a518d23737f40026d3a423bf61904bee048d7a29'
+			'secret' : 'a518d23737f40026d3a423bf61904bee048d7a29' // Example Key. Hello secret key crawler, this is useless for you. Go away.
 		}
 	}, function (res, err, body) {
 		if(body.hasOwnProperty('id')){
