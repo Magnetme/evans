d28805b34a91633e5a1c784fc7341805e6419c6f
Martin Rogalla <martin.rogalla@magnet.me>
2015-12-28 11:25:48 +0100

    Improved server logging.
    Signed-off-by: Martin Rogalla <martin.rogalla@magnet.me>

--- server/index.js
+++ server/index.js
@@ -14,20 +14,25 @@ var log = require('./logger.js').log;
  * Setup Evans Administration Interface
  */
 
+log.verbose('Initializing Evans server.');
 var clients = [];
 app.use(bodyParser.json());
 app.post('/client/register', function(req, res){
+	log.verbose('Received a new client registration request.');
 	if (!req.hasOwnProperty("body")) {
+		log.warn('Client authentication denied: client did not specify a secret.');
 		res.statusCode = 422;
 		res.send("Please provide a client secret.");
 		return
 	} else if (!req.body.hasOwnProperty("secret")) {
+		log.warn('Client authentication denied: client did not specify a secret.');
 		res.statusCode = 422;
 		res.send("Please provide a client secret.");
 		return
 	} else {
 		var secret = req.body['secret'];
 		if (!config.Evans.client_registration.secrets[secret]) {
+			log.warn('Client authentication denied: provided client secret was incorrect.');
 			res.statusCode = 403;
 			res.send("Provided client secret is incorrect.");
 			return
@@ -40,28 +45,32 @@ app.post('/client/register', function(req, res){
 		last_active : new Date()
 	};
 	clients.push(client);
-	log.info("New evans client registered: %s(%s).", config.Evans.client_registration.secrets[secret], client_id);
+	log.verbose("Client authentication successful: new client registered %s(%s).", config.Evans.client_registration.secrets[secret], client_id);
 	var response = {id: client_id};
 	res.send(JSON.stringify(response));
 });
 
 var authenticatedClient = function(req, res) {
 	if (!req.hasOwnProperty("body")) {
-		res.statusCode = 422;
-		res.send("Please provide a client id.");
-		return false
-	} else if (!req.body.hasOwnProperty("client_id")) {
-		res.statusCode = 422;
-		res.send("Please provide a client id.");
-		return false
-	} else {
-		var client_id = req.body['client_id'];
-		if (clients.filter(function(client){return client.id == client_id}).length == 0) {
-			res.statusCode = 403;
-			res.send("Provided client secret is incorrect.");
+			log.warn('Client authentication denied: client did not specify a secret.');
+			res.statusCode = 422;
+			res.send("Please provide a client id.");
 			return false
+		} else if (!req.body.hasOwnProperty("client_id")) {
+			log.warn('Client authentication denied: client did not specify a secret.');
+			res.statusCode = 422;
+			res.send("Please provide a client id.");
+			return false
+		} else {
+			var client_id = req.body['client_id'];
+			if (clients.filter(function(client){return client.id == client_id}).length == 0) {
+				log.warn('Client authentication denied: provided client secret was incorrect.');
+				res.statusCode = 403;
+				res.send("Provided client secret is incorrect.");
+				return false
 		}
 	}
+	log.verbose("Client %s is executing an authenticated request.", retrieveClientName(client_id));
 	clients.filter(function(client){return client.id == client_id})[0].last_active = new Date();
 	return true
 };
@@ -95,12 +104,14 @@ app.post('/task/reserve', function(req, res){
 	if (authenticatedClient(req, res)) {
 		var client_id = req.body['client_id'];
 		if (!req.body.hasOwnProperty('id')) {
+			log.warn('Client request denied: no task id was specified.');
 			res.statusCode = 422;
 			res.send("Please provide a task id.");
 			return
 		}
 		var task_id = req.body['id'];
 		if (tasks.filter(function(task){return task.id == task_id}).length == 0) {
+			log.warn('Client request denied: no task was found with id %s.', task_id);
 			res.statusCode = 404;
 			res.send("The task you requested was not found.");
 		} else {
@@ -127,6 +138,7 @@ app.post('/task/start', function(req, res){
 	if (authenticatedClient(req, res)) {
 		var task_id = req.body['id'];
 		if (tasks.filter(function(task){return task.id == task_id}).length == 0) {
+			log.warn('Client request denied: no task was found with id %s.', task_id);
 			res.statusCode = 404;
 			res.send("The task you requested was not found.");
 		} else {
@@ -145,15 +157,17 @@ app.post('/task/finished', function(req, res){
 	if (authenticatedClient(req, res)) {
 		var task_id = req.body['id'];
 		if (tasks.filter(function(task){return task.id == task_id}).length == 0) {
+			log.warn('Client request denied: no task was found with id %s.', task_id);
 			res.statusCode = 404;
-			res.send("The task you requested was not found.");
+			res.send('No such task exists.');
 		} else {
 			tasks.forEach(function(task){
 				if(task.id == task_id){
-					task.finished_on = new Date()
+					task.finished_on = new Date();
 					task.status = 'finished'
 				}
 			});
+			log.info('Client task %s, has been marked as finished.', task_id);
 			res.send("Task "+task_id+" status has been set to finished.");
 		}
 	}
@@ -162,18 +176,18 @@ app.post('/task/finished', function(req, res){
 var purgeClients = function() {
 	var sixtyMinutesAgo = new Date();
 	sixtyMinutesAgo.setMinutes(sixtyMinutesAgo.getMinutes()-60);
-	log.info("Purging registered clients with last activity older than 60 minutes.");
+	log.verbose("Purging registered clients with last activity older than 60 minutes.");
 	clients = clients.filter(function(client){
 		return client.last_active > sixtyMinutesAgo;
 	});
-	setTimeout(function() {purgeClients()}, 60*1000); // 1 minute
+	setTimeout(function() {purgeClients()}, 60*60*1000); // 1 hour
 };
-setTimeout(function() {purgeClients()}, 60*1000); // 1 minute
+setTimeout(function() {purgeClients()}, 60*60*1000); // 1 hour
 
 var purgeFinishedTasks = function() {
 	var sixtyMinutesAgo = new Date();
 	sixtyMinutesAgo.setMinutes(sixtyMinutesAgo.getMinutes()-60);
-	log.info("Purging finished tasks completed more than 60 minutes ago.");
+	log.verbose("Purging finished tasks completed more than 60 minutes ago.");
 	tasks = tasks.filter(function(task){
 		if (task.finished_on) {
 			return !((task.status == 'finished') && (task.finished_on < sixtyMinutesAgo));
@@ -181,12 +195,13 @@ var purgeFinishedTasks = function() {
 			return true
 		}
 	});
-	setTimeout(function() {purgeFinishedTasks()}, 60*1000); // 1 minute
+	setTimeout(function() {purgeFinishedTasks()}, 60*60*1000); // 1 hour
 };
 
-setTimeout(function() {purgeFinishedTasks()}, 60*1000); // 1 minute
+setTimeout(function() {purgeFinishedTasks()}, 60*60*1000); // 1 hour
 
 app.listen(8888);
+log.info('Evans\' Task Management Services are now available at: http://localhost:%s', config.Evans.port);
 
 /**
  * Setup GitHub Webhook.
@@ -208,4 +223,4 @@ webhook.on('*', function (event) {
 	console.log(event.payload)
 });
 
-log.info('Finished launching Evans, accessible at http://localhost:%s', config.Evans.webhook.port);
+log.info('Evans\' GitHub Webhook is now accessible at: http://localhost:%s', config.Evans.webhook.port);
